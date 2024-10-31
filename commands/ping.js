e04b15f9cc85f61bf338cddfbe9581a6a857c46c
DerGamer009 <pjager185@gmail.com>
2024-10-31 20:36:23 +0100

    #0001

--- commands/ping.js
+++ commands/ping.js
@@ -5,6 +5,6 @@ module.exports = {
     .setName('ping')
     .setDescription('Antwortet mit "Pong!"'),
   async execute(interaction) {
-    await interaction.reply('Pong!');
+    await interaction.reply('Pong!!');
   },
 };
